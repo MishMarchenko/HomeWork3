0502de4062cd71eb568ef223d37ce03f8bf930e6
MishMarchenko <mishmar4enko@gmail.com>
2023-06-13 22:43:11 +0300

    Adding part home work

--- src/Main.java
+++ src/Main.java
@@ -18,15 +18,14 @@ public class Main {
                 System.out.println(cubNumber());
                 break;
             case 4:
-                System.out.println("tbd4");
+                calculator();
                 break;
             default:
                 System.out.println("There was no such task)))");
-
         }
-
-           }
-           public static String unicode(){
+    }
+    public static String unicode(){
+        System.out.println("Let me show you some symbols");
         char q = '\u2230';
         char w = '\u2222';
         char e = '\u2205';
@@ -39,5 +38,42 @@ public class Main {
         int a = scanner.nextInt();
         return (int) Math.pow(a, 3);
 // 2й способ: return a * a * a;
+    }
+    public static void calculator(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Calculator!");
+        System.out.println("I'm simple, so I can only do simple operations!");
+        System.out.println("So let start!");
+        System.out.println();
+        System.out.println("Add first number");
+        int a = scanner.nextInt();
+        System.out.println("Add second number");
+        int b = scanner.nextInt();
+        System.out.println("Add calculation operator(+,-,*,/)");
+        char op = scanner.next().charAt(0);
+
+        if (op == '+'){
+            System.out.println(sum(a, b));
+        } else if (op == '-') {
+            System.out.println(minus(a, b));
+        } else if (op == '*') {
+            System.out.println(mult(a, b));
+        } else if (op == '/') {
+            System.out.println(divide(a, b));
+        }else {
+            System.out.println("Sorry! I don't know this operator");
+        }
+    }
+    public static int sum(int a, int b){
+        return a + b;
+    }
+    public static int minus(int a, int b){
+        return a - b;
+    }
+    public static int mult(int a, int b){
+        return a * b;
+    }
+    public static double divide(double a, double b){
+        return a / b;
     }
 }
